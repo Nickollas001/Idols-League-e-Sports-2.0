87ba3bd34f41ab09ee73b80cc627d17a40ee4266
Nickollas <nickollas001@users.noreply.github.com>
2022-04-18 18:14:51 -0300

    att

---
+++ script.js
@@ -0,0 +1,45 @@
+
+var cardSenna = document.querySelector("#Card-Senna");
+
+cardSenna.addEventListener("mouseover",function(){
+    var imagemCardSenna = document.querySelector(".categoria-card-imagem");
+    imagemCardSenna.style.height= "250px";
+});
+cardSenna.addEventListener("mouseout",function(){
+    var imagemCardSenna = document.querySelector(".categoria-card-imagem");
+    imagemCardSenna.style.height= "300px";
+});
+///
+var cardLauda = document.querySelector("#Card-Lauda");
+
+cardLauda.addEventListener("mouseover",function(){
+    var imagemCardLauda = document.querySelector(".categoria-card-imagem2");
+    imagemCardLauda.style.height= "250px";
+});
+cardLauda.addEventListener("mouseout",function(){
+    var imagemCardLauda = document.querySelector(".categoria-card-imagem2");
+    imagemCardLauda.style.height= "300px";
+});
+//
+var cardVolante = document.querySelector("#Card-Volante");
+
+cardVolante.addEventListener("mouseover",function(){
+    var imagemCardVolante = document.querySelector(".categoria-card-imagem3");
+    imagemCardVolante.style.height= "250px";
+});
+cardVolante.addEventListener("mouseout",function(){
+    var imagemCardVolante = document.querySelector(".categoria-card-imagem3");
+    imagemCardVolante.style.height= "300px";
+});
+//
+
+var cardF2 = document.querySelector("#Card-F2");
+
+cardF2.addEventListener("mouseover",function(){
+    var imagemCardF2 = document.querySelector(".categoria-card-imagem4");
+    imagemCardF2.style.height= "250px";
+});
+cardF2.addEventListener("mouseout",function(){
+    var imagemCardF2 = document.querySelector(".categoria-card-imagem4");
+    imagemCardF2.style.height= "300px";
+});
