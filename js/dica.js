670f18242cd79b1a84ee4118484239287462d134
Nickollas <nickollas001@users.noreply.github.com>
2022-04-21 18:49:52 -0300

    att

--- js/dica.js
+++ js/dica.js
@@ -1,30 +1,6 @@
-var cardDica0 = document.querySelector("#card-dica0");
-var cardDica1 = document.querySelector("#card-dica1");
-var cardDica2 = document.querySelector("#card-dica2");
-var cardDicaImagem = document.querySelector(".card-dica-img");
-
-cardDica0.addEventListener("mouseover",function(){
-    cardDica0.style.color= "white";
-    cardDica0.style.background= "#0090D0";
-});
-
-cardDica0.addEventListener("mouseout",function(){
-    cardDica0.style.color= "black";
-    cardDica0.style.background= "#00314d";
-});
-cardDica1.addEventListener("mouseover",function(){
-    cardDica1.style.color= "white";
-    cardDica1.style.background= "#0090D0";
-});
-cardDica1.addEventListener("mouseout",function(){
-    cardDica1.style.color= "black";
-    cardDica1.style.background= "#00314d";
-});
-cardDica2.addEventListener("mouseover",function(){
-    cardDica2.style.color= "white";
-    cardDica2.style.background= "#0090D0";
-});
-cardDica2.addEventListener("mouseout",function(){
-    cardDica2.style.color= "black";
-    cardDica2.style.background= "#00314d";
+var austria19 = document.getElementById("id1");
+austria19.addEventListener("click",function(){
+    var austria19info = document.getElementById("info-circuit-pista-piloto-austria");
+    austria19info.classList.toggle("hide");
+        
 });
