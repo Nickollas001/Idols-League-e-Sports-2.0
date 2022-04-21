c0c935726055d0a44f21595f9114d60e88e56275
Nickollas <nickollas001@users.noreply.github.com>
2022-04-21 20:46:39 -0300

    att

--- js/dica.js
+++ js/dica.js
@@ -1,6 +1,6 @@
-var austria19 = document.getElementById("id1");
-austria19.addEventListener("click",function(){
-    var austria19info = document.getElementById("info-circuit-pista-piloto-austria");
-    austria19info.classList.toggle("hide");
-        
+var russia19 = document.getElementById("idrussia");
+russia19.addEventListener("click",function(){
+    russia19.classList.toggle("selecionado");
+    var russia19info = document.getElementById("info-circuit-pista-piloto-russia");
+    russia19info.classList.toggle("hide");     
 });
