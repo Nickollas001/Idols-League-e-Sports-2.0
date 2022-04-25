4ae873da493ffc27c0508ea877ddc2734883b74d
Nickollas <nickollas001@users.noreply.github.com>
2022-04-24 23:42:31 -0300

    att

---
+++ root.js
@@ -0,0 +1,51 @@
+//Váriaveis posições de corrida e sprint
+one_position_race = '25';
+two_position_race = '18';
+three_position_race = '15';
+for_position_race = '12';
+fice_position_race = '10';
+six_position_race = '08';
+seven_position_race = '06';
+eight_position_race = '04';
+nine_position_race = '02';
+ten_position_race = '01';
+onze_position_race = '00';
+doze_position_race = '00';
+treze_position_race = '00';
+catorze_position_race = '00';
+quinze_position_race = '00';
+dezesseis_position_race = '00';
+dezessete_position_race = '00';
+dezoito_position_race = '00';
+dezenove_position_race = '00';
+vinte_position_race = '00';
+//Let's go Sprint
+one_position_sprint = '15';
+two_position_sprint = '12';
+three_position_sprint = '10';
+for_position_sprint = '08';
+fice_position_sprint = '06';
+six_position_sprint = '04';
+seven_position_sprint = '02';
+eight_position_sprint = '01';
+nine_position_sprint = '00';
+ten_position_sprint = '00';
+onze_position_sprint = '00';
+doze_position_sprint = '00';
+treze_position_sprint = '00';
+catorze_position_sprint = '00';
+quinze_position_sprint = '00';
+dezesseis_position_sprint = '00';
+dezessete_position_sprint = '00';
+dezoito_position_sprint = '00';
+dezenove_position_sprint = '00';
+vinte_position_sprint = '00';
+//pontos extras
+pole_race = '00';
+pole_sprint = '04';
+vmr_race = '02';
+vmr_sprint = '02';
+pd_race = '02';
+pd_sprint = '00';
+
+console.log(one_position_race);
