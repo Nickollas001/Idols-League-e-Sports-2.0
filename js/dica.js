d6733be2f5903ca3106a6bcd09e5df920a7e8da1
Nickollas <nickollas001@users.noreply.github.com>
2022-04-30 14:03:27 -0300

    att

--- js/dica.js
+++ js/dica.js
@@ -4,6 +4,13 @@ russia19.addEventListener("click",function(){
     var russia19info = document.getElementById("info-circuit-pista-piloto-russia");
     russia19info.classList.toggle("hide");     
 });
+var tabela_circuitos_2019 = document.getElementById("tabela-circuitos-2019");
+var f1_2019 = document.getElementById("f1-2019");
+    f1_2019.addEventListener("click",function(){
+        tabela_circuitos_2019.classList.toggle('hide');
+});
+
+
 var menuBar = document.querySelector('.container-menu-mob');
 
 menuBar.addEventListener('click',(e)=>{
@@ -18,3 +25,4 @@ menuBar.addEventListener('click',(e)=>{
 function myFunction(x) {
     x.classList.toggle("change");
   }
+ console.log(tabela_circuitos_2019)
