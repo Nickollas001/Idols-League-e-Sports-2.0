943c124fa85af792a566c389566bf49b3eebac34
Nickollas <nickollas001@users.noreply.github.com>
2022-08-09 16:44:31 -0300

    att

--- js/etapas.js
+++ js/etapas.js
@@ -1,9 +1,14 @@
 var australiaDownloadTableSenna = document.getElementById("australia-download-table-senna");
+var monzaDownloadTableSenna = document.getElementById("monza-download-table-senna");
 var historicoAustraliaSenna = document.getElementById("australia-historico-senna");
 
 
 australiaDownloadTableSenna.addEventListener("click",function(){
     window.location = '../tabelas/sennat8-etapa1.png'
+
+})
+monzaDownloadTableSenna.addEventListener("click",function(){
+    window.location = '../tabelas/sennat8-etapa2.png'
 
 })
 
