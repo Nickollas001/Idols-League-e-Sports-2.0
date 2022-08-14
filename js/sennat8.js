b4d02badbb413e5a0260397036c031707bed4546
Nickollas <nickollas001@users.noreply.github.com>
2022-08-14 13:25:35 -0300

    att

--- js/sennat8.js
+++ js/sennat8.js
@@ -1,12 +1,2 @@
-var vizualizarTabela = document.getElementById("vizualizarTabela");
-
-vizualizarTabela.addEventListener("click",function(){
-    let tabela = document.getElementById('tabela');
-    if(tabela.classList.contains('show-tabela')){
-        tabela.classList.remove('show-tabela');
-    }else{
-        tabela.classList.add('show-tabela');
-    }
-});
 
 
