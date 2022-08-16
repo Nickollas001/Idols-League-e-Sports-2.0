64cf4bdcbbc4a8735daa11696e687a0a6141dc93
Nickollas <nickollas001@users.noreply.github.com>
2022-08-16 01:29:53 -0300

    att

--- js/etapas.js
+++ js/etapas.js
@@ -1,4 +1,5 @@
 var australiaDownloadTableSenna = document.getElementById("australia-download-table-senna");
+var singapuraDownloadTableSenna = document.getElementById("singapura-download-table-senna");
 var monzaDownloadTableSenna = document.getElementById("monza-download-table-senna");
 var historicoAustraliaSenna = document.getElementById("australia-historico-senna");
 
@@ -9,6 +10,10 @@ australiaDownloadTableSenna.addEventListener("click",function(){
 })
 monzaDownloadTableSenna.addEventListener("click",function(){
     window.location = '../tabelas/sennat8-etapa2.png'
+
+})
+singapuraDownloadTableSenna.addEventListener("click",function(){
+    window.location = '../tabelas/sennat8-etapa3.png'
 
 })
 
--- script.js
+++ script.js
@@ -76,7 +76,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const nextRacing = "15 aug 2022 21:30:00 GMT-0300";
+const nextRacing = "22 aug 2022 21:30:00 GMT-0300";
 
 function countdown(){
     const nextRacingDate = new Date(nextRacing);
