0ba3f1fa9b19a3056e6690329e6dad966da05b56
Nickollas <nickollas001@users.noreply.github.com>
2022-08-19 12:50:20 -0300

    att

--- js/lauda.js
+++ js/lauda.js
@@ -1,7 +1,7 @@
 
 const nextRacingBrasil = "18 aug 2022 21:30:00 GMT-0300";
-const nextRacingBelgica = "18 aug 2022 21:30:00 GMT-0300";
-const nextRacingImola = "18 aug 2022 21:30:00 GMT-0300";
+const nextRacingBelgica = "25 aug 2022 21:30:00 GMT-0300";
+const nextRacingImola = "01 sep 2022 21:30:00 GMT-0300";
 const nextRacingHungria = "08 sep 2022 21:30:00 GMT-0300";
 const nextRacingAustria = "15 sep 2022 21:30:00 GMT-0300";
 const nextRacingEspanha = "22 sep 2022 21:30:00 GMT-0300";
@@ -27,12 +27,13 @@ const date_etapa4 = "01 Setembro";
 const date_etapa5 = "08 Setembro";
 const date_etapa6 = "15 Setembro";
 const date_etapa7 = "22 Setembro";
+const date_etapa8 = "29 Setembro";
 
 const brasil = "BRASIL"
 const belgica = "BÉLGICA"
 const imola = "IMOLA"
 const hungria = "HUNGRIA"
-const austria = "ÁUSTRIA"
+const austria = "AUSTRIA"
 const espanha = "ESPANHA"
 const italia = "ITÁLIA"
 
@@ -76,6 +77,36 @@ if (currentDate < nextRacingDateBrasil){
         nextEtapa.innerHTML = hungria;
         dateEtapa.innerHTML = date_etapa5;
         container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Hungary.jpg.transform/12col-retina/image.jpg')"
+        container.style.backgroundSize = "cover"
+        container.style.backgroundPosition = "center";
+    }
+    countdown();
+    setInterval(countdown,1000); 
+}else if (currentDate < nextRacingDateAustria){
+    function countdown(){
+        nextEtapa.innerHTML = austria;
+        dateEtapa.innerHTML = date_etapa6;
+        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Austria.jpg.transform/12col-retina/image.jpg')"
+        container.style.backgroundSize = "cover"
+        container.style.backgroundPosition = "center";
+    }
+    countdown();
+    setInterval(countdown,1000); 
+}else if (currentDate < nextRacingDateEspanha){
+    function countdown(){
+        nextEtapa.innerHTML = espanha;
+        dateEtapa.innerHTML = date_etapa7;
+        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Barcelona%20Test.jpg.transform/12col-retina/image.jpg')"
+        container.style.backgroundSize = "cover"
+        container.style.backgroundPosition = "center";
+    }
+    countdown();
+    setInterval(countdown,1000); 
+}else if (currentDate < nextRacingDateItalia){
+    function countdown(){
+        nextEtapa.innerHTML = italia;
+        dateEtapa.innerHTML = date_etapa8;
+        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Italy.jpg.transform/12col-retina/image.jpg')"
         container.style.backgroundSize = "cover"
         container.style.backgroundPosition = "center";
     }
