30894986d69245488570cb2c41a57d5a46190b2d
Nickollas <nickollas001@users.noreply.github.com>
2022-08-19 12:38:34 -0300

    att

--- js/lauda.js
+++ js/lauda.js
@@ -1,7 +1,7 @@
 
 const nextRacingBrasil = "18 aug 2022 21:30:00 GMT-0300";
-const nextRacingBelgica = "25 aug 2022 21:30:00 GMT-0300";
-const nextRacingImola = "01 sep 2022 21:30:00 GMT-0300";
+const nextRacingBelgica = "18 aug 2022 21:30:00 GMT-0300";
+const nextRacingImola = "18 aug 2022 21:30:00 GMT-0300";
 const nextRacingHungria = "08 sep 2022 21:30:00 GMT-0300";
 const nextRacingAustria = "15 sep 2022 21:30:00 GMT-0300";
 const nextRacingEspanha = "22 sep 2022 21:30:00 GMT-0300";
@@ -30,9 +30,11 @@ const date_etapa7 = "22 Setembro";
 
 const brasil = "BRASIL"
 const belgica = "BÉLGICA"
-const silverstone = "GRÃ BETANHA"
-const eua = "ESTADOS UNIDOS"
-const abudhabi = "ABU DHABI"
+const imola = "IMOLA"
+const hungria = "HUNGRIA"
+const austria = "ÁUSTRIA"
+const espanha = "ESPANHA"
+const italia = "ITÁLIA"
 
 const container = document.getElementById("container");
 
@@ -54,6 +56,26 @@ if (currentDate < nextRacingDateBrasil){
         nextEtapa.innerHTML = belgica;
         dateEtapa.innerHTML = date_etapa3;
         container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Belgium.jpg.transform/12col-retina/image.jpg')"
+        container.style.backgroundSize = "cover"
+        container.style.backgroundPosition = "center";
+    }
+    countdown();
+    setInterval(countdown,1000); 
+}else if (currentDate < nextRacingDateImola){
+    function countdown(){
+        nextEtapa.innerHTML = imola;
+        dateEtapa.innerHTML = date_etapa4;
+        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Emilia%20Romagna.jpg.transform/12col-retina/image.jpg')"
+        container.style.backgroundSize = "cover"
+        container.style.backgroundPosition = "center";
+    }
+    countdown();
+    setInterval(countdown,1000); 
+}else if (currentDate < nextRacingDateHungria){
+    function countdown(){
+        nextEtapa.innerHTML = hungria;
+        dateEtapa.innerHTML = date_etapa5;
+        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Hungary.jpg.transform/12col-retina/image.jpg')"
         container.style.backgroundSize = "cover"
         container.style.backgroundPosition = "center";
     }
