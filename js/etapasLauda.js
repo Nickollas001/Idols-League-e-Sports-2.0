3fa7ca6c78d9ddba305ba03d1893bdcfc5925aaa
Nickollas <nickollas001@users.noreply.github.com>
2022-09-16 10:14:55 -0300

    att

---
+++ js/etapasLauda.js
@@ -0,0 +1,24 @@
+var hungriaDownloadTableLauda = document.getElementById("download-table-f2-hungria");
+var imolaDownloadTableLauda = document.getElementById("download-table-f2-imola");
+var brasilDownloadTableLauda = document.getElementById("download-table-f2-brasil");
+var miamiDownloadTableLauda = document.getElementById("download-table-f2-miami");
+
+
+
+
+hungriaDownloadTableLauda.addEventListener("click",function(){
+    window.location = '../tabelas/F2-etapa5.png'
+
+});
+imolaDownloadTableLauda.addEventListener("click",function(){
+    window.location = '../tabelas/F2-etapa4.png'
+
+});
+brasilDownloadTableLauda.addEventListener("click",function(){
+    window.location = '../tabelas/F2-etapa2.png'
+
+});
+miamiDownloadTableLauda.addEventListener("click",function(){
+    window.location = '../tabelas/F2-etapa1.png'
+
+});
