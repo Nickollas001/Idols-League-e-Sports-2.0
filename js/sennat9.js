070886eea4639e8c178e3d2edddbc6e072924147
Nickollas <nickollas001@users.noreply.github.com>
2022-10-25 19:30:00 -0300

    att

--- js/sennat9.js
+++ js/sennat9.js
@@ -1,12 +1,14 @@
 
-const nextRacingBrasil = "10 aug 2022 21:30:00 GMT-0300";
-const nextRacingMiami = " 29 aug 2022 21:30:00 GMT-0300";
-const nextRacingSilverstone = "05 sep 2022 21:30:00 GMT-0300";
-const nextRacingEua = "12 sep 2022 21:30:00 GMT-0300";
-const nextRacingDateBrasil = new Date(nextRacingBrasil);
-const nextRacingDateSilverstone = new Date(nextRacingSilverstone);
-const nextRacingDateMiami = new Date(nextRacingMiami);
-const nextRacingDateEua = new Date(nextRacingEua);
+const nextRacingHolanda = "31 oct 2022 21:30:00 GMT-0300";
+const nextRacingBelgica = "07 nov 2022 21:30:00 GMT-0300";
+const nextRacingJapao = " 14 nov 2022 21:30:00 GMT-0300";
+const nextRacingAustria = "21 nov 2022 21:30:00 GMT-0300";
+const nextRacingEspanha = "28 nov 2022 21:30:00 GMT-0300";
+const nextRacingDateHolanda = new Date(nextRacingHolanda);
+const nextRacingDateBelgica = new Date(nextRacingBelgica);
+const nextRacingDateAustria = new Date(nextRacingAustria);
+const nextRacingDateJapao = new Date(nextRacingJapao);
+const nextRacingDateEspanha = new Date(nextRacingEspanha);
 const currentDate = new Date();
 
 
@@ -15,18 +17,26 @@ const dateEtapa = document.getElementById("date_etapa");
 const imagemEtapa = document.getElementById("imagem_etapa");
 
 
-const date_etapa4 = "22 Agosto";
-const date_etapa5 = "29 Agosto";
-const date_etapa6 = "05 Setembro";
-const date_etapa7 = "12 Setembro";
-const date_etapa8 = "17 Outubro";
+const date_etapa3 = "31 Outubro";
+const date_etapa4 = "07 Novembro";
+const date_etapa5 = "14 Novembro";
+const date_etapa6 = "21 Novembro";
+const date_etapa7 = "28 Novembro";
+const date_etapa8 = "05 Dezembro";
 
-const brasil = "BRASIL"
-const hungria = "HUNGRIA"
-const miami = "MIAMI"
-const silverstone = "GRÃ BRETANHA"
-const eua = "ESTADOS UNIDOS"
-const abudhabi = "ABU DHABI"
+const brasil = "BRASIL";
+const franca = "FRANÇA";
+const holanda = "HOLANDA";
+const belgica = "BÉLGICA";
+const japao = "JAPÃO";
+const austria = "ÁUSTRIA";
+const espanha = "ESPANHA";
+const bahrein = "BAHREIN";
+const hungria = "HUNGRIA";
+const miami = "MIAMI";
+const silverstone = "GRÃ BRETANHA";
+const eua = "ESTADOS UNIDOS";
+const abudhabi = "ABU DHABI";
 
 const container = document.getElementById("container");
 
@@ -36,38 +46,51 @@ function formatTime(time){
     return time < 10 ? `0${time}` :time;
 }
 
-if (currentDate < nextRacingDateBrasil){
+if (currentDate < nextRacingDateHolanda){
     function countdown(){
-        nextEtapa.innerHTML = brasil;
+        nextEtapa.innerHTML = holanda;
+        dateEtapa.innerHTML = date_etapa3;
+        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Netherlands.jpg.transform/12col/image.jpg')"
+        container.style.backgroundSize = "cover"
+        container.style.backgroundPosition = "center";
+    }
+    countdown();
+    setInterval(countdown,1000); 
+}else if (currentDate < nextRacingDateBelgica){
+    function countdown(){
+        nextEtapa.innerHTML = belgica;
         dateEtapa.innerHTML = date_etapa4;
+        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Belgium.jpg.transform/12col/image.jpg')"
+        container.style.backgroundSize = "cover"
+        container.style.backgroundPosition = "center";
     }
     countdown();
     setInterval(countdown,1000); 
-}else if (currentDate < nextRacingDateMiami){
+}else if (currentDate < nextRacingDateJapao){
     function countdown(){
-        nextEtapa.innerHTML = miami;
+        nextEtapa.innerHTML = japao;
         dateEtapa.innerHTML = date_etapa5;
-        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Miami.jpg.transform/12col-retina/image.jpg')"
+        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Japan.jpg.transform/12col/image.jpg')"
         container.style.backgroundSize = "cover"
         container.style.backgroundPosition = "center";
     }
     countdown();
     setInterval(countdown,1000); 
-}else if (currentDate < nextRacingDateSilverstone){
+}else if (currentDate < nextRacingDateAustria){
     function countdown(){
-        nextEtapa.innerHTML = silverstone;
+        nextEtapa.innerHTML = austria;
         dateEtapa.innerHTML = date_etapa6;
-        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Great%20Britain.jpg.transform/12col-retina/image.jpg')"
+        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Austria.jpg.transform/12col/image.jpg')"
         container.style.backgroundSize = "cover"
         container.style.backgroundPosition = "center";
     }
     countdown();
     setInterval(countdown,1000); 
-}else if (currentDate < nextRacingDateEua){
+}else if (currentDate < nextRacingDateEspanha){
     function countdown(){
-        nextEtapa.innerHTML = eua;
+        nextEtapa.innerHTML = espanha;
         dateEtapa.innerHTML = date_etapa7;
-        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/USA.jpg.transform/12col-retina/image.jpg')"
+        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Barcelona%20Test.jpg.transform/12col/image.jpg')"
         container.style.backgroundSize = "cover"
         container.style.backgroundPosition = "center";
     }
@@ -75,9 +98,9 @@ if (currentDate < nextRacingDateBrasil){
     setInterval(countdown,1000); 
 }else{
     function countdown(){
-        nextEtapa.innerHTML = hungria;
+        nextEtapa.innerHTML = bahrein;
         dateEtapa.innerHTML = date_etapa8;
-        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Hungary.jpg.transform/12col/image.jpg')"
+        container.style.background = "url('https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Bahrain.jpg.transform/12col/image.jpg')"
         container.style.backgroundSize = "cover"
         container.style.backgroundPosition = "center";
     }
